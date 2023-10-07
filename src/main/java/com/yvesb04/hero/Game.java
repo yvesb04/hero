c9cfd6bd2221538aecafc0cdc6898c839abddabe
yvesb04 <yves.bonneau2004@gmail.com>
2023-10-07 15:10:37 +0100

    Reading KeyStrokes

--- src/main/java/com/yvesb04/hero/Game.java
+++ src/main/java/com/yvesb04/hero/Game.java
@@ -2,6 +2,8 @@ package com.yvesb04.hero;
 
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -12,8 +14,10 @@ import java.io.IOException;
 public class Game {
 
     private final TerminalScreen screen;
-    Game() throws IOException {
+    private int x = 20;
+    private int y = 10;
 
+    Game() throws IOException {
         TerminalSize terminalSize = new TerminalSize(40, 20);
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
 
@@ -27,12 +31,43 @@ public class Game {
 
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
-     void run() throws IOException {
-        draw();
+
+    private void processKey(KeyStroke key) throws IOException {
+        System.out.println(key);
+        switch (key.getKeyType()) {
+            case ArrowUp:
+               y--;
+               break;
+            case ArrowDown:
+                y++;
+                break;
+            case ArrowLeft:
+                x--;
+                break;
+            case ArrowRight:
+                x++;
+                break;
+            case Character:
+                if (key.getCharacter() == 'q') {
+                    screen.close();
+                }
+            case EOF:
+                screen.close();
+        }
+
     }
 
-
+    void run() throws IOException {
+        while (true) {
+            draw();
+            KeyStroke key = screen.readInput();
+            processKey(key);
+            if (key.getKeyType() == KeyType.EOF) {
+                break;
+            }
+        }
+    }
 }
